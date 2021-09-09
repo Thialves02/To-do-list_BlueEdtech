a63f30e9f2ef064028da279ed365ce6942783592
Thiago <thiago.ralves02@gmail.com>
2021-09-09 14:58:42 -0300

    menu inicializado

---
+++ Frontend/to-do-list/src/components/Menu/Menu.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Menu.css'
+
+const Menu = () =>{
+    return(
+        <nav className="Navbar">
+            <div className="Menu">
+                <h1>Options</h1>
+                <h2><i class="fas fa-user-alt"></i>Account</h2>
+                <h2><i class="fas fa-cog"></i>Settings</h2>
+                <h2> <i class="fas fa-adjust"></i>DarkMode</h2>
+                <h1>Workspace</h1>
+                <h2>Project 0</h2>
+            </div> 
+            <div className="div-button">
+                <button><i class="fas fa-plus"></i>New Project</button>
+            </div> 
+            
+        </nav>
+    );
+}
+
+export default Menu
