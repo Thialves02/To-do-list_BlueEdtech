a8edaff3905e077588dee638c0b6db3febfc6db0
Thiago <thiago.ralves02@gmail.com>
2021-09-09 15:15:42 -0300

    pagina home adicionada

--- Frontend/to-do-list/src/App.js
+++ Frontend/to-do-list/src/App.js
@@ -2,15 +2,19 @@ import React from 'react'
 import { BrowserRouter as Router,Switch, Route } from 'react-router-dom';
 import './App.css';
 import Menu from './components/Menu/Menu';
+import Home from './pages/Home';
 
 function App() {
   return (
+    <div className="App">
     <Router>
       <Menu/>
       <Switch>
-        <Route/>
+        <Route path='/' exact component={Home}/>
       </Switch>
     </Router>
+    </div>
+    
   );
 }
 
---
+++ Frontend/to-do-list/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Home.css';
+
+const Home = () =>{
+    return(
+        <>
+        <h1>BEM VINDO A PÁGINA HOME</h1>
+        </>
+    );
+}
+
+export default Home;
